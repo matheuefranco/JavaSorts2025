eb0330ea5eb11ed772745f74e192700167cb6ee7
matheuefranco <109219754+matheusfrancoifmachado@users.noreply.github.com>
2025-08-11 10:17:39 -0300

    feat:classe-pessoa

--- src/App.java
+++ src/App.java
@@ -1,22 +1,26 @@
+import java.time.LocalDate;
 import java.util.*;
 public class App {
+
+     public static Pessoa[] vetorDePessoas() {
+        return new Pessoa[]{
+            new Pessoa("João", LocalDate.of(1990, 1, 1)),
+            new Pessoa("Ana", LocalDate.of(1992, 2, 2)),
+            new Pessoa("Maria", LocalDate.of(1995, 5, 5)),
+            new Pessoa("Pedro", LocalDate.of(1993, 4, 4)),
+            new Pessoa("Lucas", LocalDate.of(1991, 6, 6)),
+            new Pessoa("Carlos", LocalDate.of(1988, 3, 3))
+        };
+    }
     public static void main(String[] args)  {
         Scanner leia = new Scanner(System.in);
-        Random rand = new Random();
-        System.out.println("Tamanho do vetor:");
-        int n = leia.nextInt();
-        Integer[] vetor = new Integer[n];
-        for (int i=0;i<n;i++)
-            vetor[i] = rand.nextInt(100);
-
-        System.out.println("Antes de ordenar:"+ 
-                         Arrays.toString(vetor) );    
-        BubbleSort<Integer> bsort = new BubbleSort<>();
-        Comparator<Integer> comparaInt = (a,b) -> a.compareTo(b);
-        bsort.sort(vetor, true);
-       
-        System.out.println("Depois de ordenar:"+ 
-                         Arrays.toString(vetor) ); 
-        
+        Pessoa[] vetorPessoas = vetorDePessoas(); 
+        System.out.println("Pessoas antes de ordenar:"+
+              Arrays.toString(vetorPessoas));
+        BubbleSort<Pessoa> bsortPessoa = new BubbleSort<>();
+        //Criar comparator por data de nascimento
+        bsortPessoa.sort(vetorPessoas);
+        System.out.println("Pessoas depois de ordenar:"+
+              Arrays.toString(vetorPessoas)); 
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,62 @@
+import java.time.LocalDate;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                '}';
+    }
+    
+   
+
+    //implementar comparable compareto  
+    @Override
+    public int compareTo(Pessoa outra) {
+        return this.nome.compareTo(outra.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pessoa other = (Pessoa) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    
+}
